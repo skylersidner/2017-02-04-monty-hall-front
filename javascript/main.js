e975538efdd224e432e6fff012607a7bdda58914
Skyler Sidner <skyler.sidner@atssecured.com>
2017-02-24 20:33:50 -0600

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+// let game1 = document.getElementById('game1');
+// let game2 = document.getElementById('game2');
+// let game3 = document.getElementById('game3');
+// let game4 = document.getElementById('game4');
+
+
+let playGame = (numberOfGames, numberOfDoors, isSwapping) => {
+    displayGameResults('Victory!');
+}
+
+let displayGameResults = (victory) => {
+    let wins = document.getElementById('wins');
+    wins.innerHTML = victory;
+}
+
+// game1.onclick = playGame(1, 3, false);
+// game2.onclick = playGame(1, 3, true);
+// game3.onclick = playGame(10, 3, false);
+// game4.onclick = playGame(10, 3, true);
