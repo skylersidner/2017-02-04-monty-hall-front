6fd4216c8c6cb124e45359ff2e452aa5a4fff5b1
Skyler Sidner <skyler.sidner@atssecured.com>
2017-02-24 22:11:45 -0600

    emptied main.js; adjusted service to use new endpoint and populate other result values in view

--- javascript/decisionEngineService.js
+++ javascript/decisionEngineService.js
@@ -1,12 +1,18 @@
 var DOMAIN = 'localhost:8080';
 
-let playGame = (numberOfGames, numberOfDoors, isSwapping) => {
+let playGame = (numberOfRounds, numberOfDoors, isSwapping) => {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://' + DOMAIN + '/engine/3doorStay', true);
+    let variables = {
+        'numberOfRounds' : numberOfRounds,
+        'numberOfDoors' : numberOfDoors,
+        'isSwapping' : isSwapping
+    };
+    xhr.open('GET', 'http://' + DOMAIN + '/engine/params?numberOfDoors=' + numberOfDoors +
+        '&isSwapping=' + isSwapping + '&numberOfRounds=' + numberOfRounds);
     xhr.onload = function() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                displayGameResults(xhr.response);
+                displayGameResults(xhr.response, variables);
             } else {
                 console.error(xhr.statusText);
             }
@@ -15,7 +21,15 @@ let playGame = (numberOfGames, numberOfDoors, isSwapping) => {
     xhr.send();
 }
 
-let displayGameResults = (result) => {
+let displayGameResults = (result, variables) => {
+    let roundsPlayed = document.getElementById('rounds-played');
+    let doorsUsed = document.getElementById('doors-used');
+    let stayOrSwap = document.getElementById('stay-or-swap');
     let wins = document.getElementById('wins');
+
+    roundsPlayed.innerHTML = variables.numberOfRounds;
+    doorsUsed.innerHTML = variables.numberOfDoors;
+    stayOrSwap.innerHTML = variables.isSwapping ? 'Swap' : 'Stay';
+
     wins.innerHTML = result;
 }
